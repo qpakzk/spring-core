bff6cf0e1f53ceb10df2272fc1d3043d40828a55
Sangwon Hong <qpakzk@gmail.com>
2020-12-08 17:30:30 +0900

    implement AppConfig

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,13 +1,20 @@
 package hello.core.member;
 
-import org.assertj.core.api.Assertions;
+import hello.core.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
